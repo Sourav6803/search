7c7683f557053a2ae1f7c4f62dcfd65d1120c84c
Sourav Bhukta <souravbhukta8@gmail.com>
2023-08-01 11:38:08 +0530

    gg

--- src/Search.jsx
+++ src/Search.jsx
@@ -9,15 +9,13 @@ import UserData from "./UserData"
 const Search = () => {
   const [data, setData] = useState([])
   const [filter, setFilter] = useState("")
+  const [ivalue, setValue] = useState("")
   const type = useRef()
 
+
   useEffect(() => {
-    let code = type.current.value
-    // console.log(code.length)
-    console.log(filter.length)
 
-
-    filter && fetch(`http://localhost:9900/filterImportData?${code}=${filter}`)
+    filter && fetch(`http://localhost:9900/filterImportData?${ivalue}=${filter}`)
       .then(res => res.json())
       .then(da => {
         console.log(da.responce)
@@ -31,7 +29,7 @@ const Search = () => {
 
     <>
 
-      <nav className="container-fluid navbar-expand-lg navbar-light m-3" style={{ backgroundColor: "", height: "60px" }}>
+      {/* <nav className="container-fluid navbar-expand-lg navbar-light m-3" style={{ backgroundColor: "", height: "60px" }}>
 
 
         <div className="d-flex  justify-content-end align-items-center " >
@@ -48,20 +46,26 @@ const Search = () => {
 
           </ul>
         </div>
-      </nav>
+      </nav> */}
 
 
 
 
       <div className="search_div " style={{ height: "35vh", backgroundImage: `url(${logo})` }}>
         <div className="mt-3">
-        <h1 className="d-flex align-items-center justify-content-center  text-white" >Search Import Export Data Of India</h1>
+          <h1 className="d-flex align-items-center justify-content-center  text-white" >Search Import Export Data Of India</h1>
+        </div>
+        <div className="radioBtn d-flex align-items-center justify-content-center ">
+           
+            <input className=" fs-1 text-white " type="radio" value="HSN_Code" name="data" onChange={e => setValue(e.target.value)} />HSN_Code
+            <input className="fs-1 text-white"  type="radio" value="Supplier_Name" name="data" onChange={e => setValue(e.target.value)} />Supplier_Name
+          
         </div>
         <div className='search'>
-          <select className="bg-warning text-white text-bold m-0 p-0" style={{ height: "8vh", width: "6vw", borderRadius: "10px" }} ref={type}>
+          {/* <select className="bg-warning text-white text-bold m-0 p-0" style={{ height: "8vh", width: "6vw", borderRadius: "10px" }} ref={type}>
             <option value="HSN_Code">HSN_Code</option>
             <option value="Supplier_Name">Supplier_Name</option>
-          </select>
+          </select> */}
           <input className="search fs-1S" type='text' placeholder='Enter HSN Code/Supplier_Name' onChange={e => setFilter(e.target.value)} />
           <button type="submit" value='search' style={{ height: "9vh", width: "6vw", borderRadius: "10px" }} className="bg-warning text-white fs-1 m-0 p-0">Search</button>
         </div>
@@ -73,13 +77,13 @@ const Search = () => {
             <h1>Detailed Import Data of {data.length}</h1>
           </div>
           <div className="row" >
-            <div className="col small d-flex align-items-center justify-content-center " style={{ height: "20vh",borderRadius: "20px" }}>
+            <div className="col small d-flex align-items-center justify-content-center " style={{ height: "20vh", borderRadius: "20px" }}>
               <div className="">
                 <h1 className="d-flex justify-content-center  ">5,150</h1>
                 <h4>Import Shipment Records found</h4>
               </div>
             </div>
-            <div className="col small d-flex align-items-center justify-content-center"  style={{ height: "20vh",borderRadius: "20px" }}>
+            <div className="col small d-flex align-items-center justify-content-center" style={{ height: "20vh", borderRadius: "20px" }}>
               <div className="">
                 <h2>Detailed Analysis & Trends of:</h2>
                 <h2 className="d-flex justify-content-center">456</h2>
@@ -88,7 +92,7 @@ const Search = () => {
                 </div>
               </div>
             </div>
-            <div className="col small  d-flex align-items-center justify-content-center"  style={{ height: "20vh",borderRadius: "20px" }}>
+            <div className="col small  d-flex align-items-center justify-content-center" style={{ height: "20vh", borderRadius: "20px" }}>
               <div>
                 <h2>Customs Import Duty of:</h2>
                 <h2 className="d-flex justify-content-center">34456</h2>
@@ -97,7 +101,7 @@ const Search = () => {
                 </div>
               </div>
             </div>
-            <div className="col small d-flex align-items-center justify-content-center "  style={{ height: "20vh",borderRadius: "20px" }}>
+            <div className="col small d-flex align-items-center justify-content-center " style={{ height: "20vh", borderRadius: "20px" }}>
               <div>
                 <h2>Trademarks on this page</h2>
                 <div className="bg-primary text-white">
@@ -110,31 +114,33 @@ const Search = () => {
         </div>
       </div>
 
-      <div className="result">
-        <table>
+      {
+        data.length && <div className="result">
+          <table>
 
-          <thead>
-            <tr>
-              <th scope="col">Bill_of_Entry_Date</th>
-              <th scope="col">Importer_Name</th>
-              <th scope="col">HSN_Code</th>
-              <th scope="col">Product_Description</th>
-              <th scope="col">Supplier_Name</th>
-              <th scope="col">Supplier_Country</th>
-              <th scope="col">Unit_Rate_in_FC</th>
-              <th scope="col">Total_Rate_in_FC</th>
-              <th scope="col">Unit</th>
-              <th scope="col">Currency</th>
-            </tr>
-          </thead>
+            <thead>
+              <tr>
+                <th scope="col">Bill_of_Entry_Date</th>
+                <th scope="col">Importer_Name</th>
+                <th scope="col">HSN_Code</th>
+                <th scope="col">Product_Description</th>
+                <th scope="col">Supplier_Name</th>
+                <th scope="col">Supplier_Country</th>
+                <th scope="col">Unit_Rate_in_FC</th>
+                <th scope="col">Total_Rate_in_FC</th>
+                <th scope="col">Unit</th>
+                <th scope="col">Currency</th>
+              </tr>
+            </thead>
 
 
-          <tbody>
-            <UserData data={data} />
-          </tbody>
+            <tbody>
+              <UserData data={data} />
+            </tbody>
 
-        </table>
-      </div>
+          </table>
+        </div>
+      }
 
     </>
   )
